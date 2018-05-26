dd10bdd147b4c4563bd418aa0c50a604f4e921b1
Evan Kirkiles <evkirkiles@gmail.com>
2018-05-25 23:59:14 -0400

    Setting up Xcode environment

---
+++ Backtest Environment/Backtest Environment/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Backtest Environment
+//
+//  Created by Evan Kirkiles on 5/25/18.
+//  Copyright © 2018 Evan Kirkiles. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
