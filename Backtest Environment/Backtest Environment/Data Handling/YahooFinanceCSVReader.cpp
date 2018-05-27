c0df6711edcdc571cbe95f78099daea5f18d6b08
Sam Kirkiles <evkirkiles@gmail.com>
2018-05-27 01:00:25 -0400

    Trying to get crumb verification working

--- Backtest Environment/Backtest Environment/Data Handling/YahooFinanceCSVReader.cpp
+++ Backtest Environment/Backtest Environment/Data Handling/YahooFinanceCSVReader.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Evan Kirkiles. All rights reserved.
 //
 
+#include <fstream>
 #include <iostream>
 #include <curl/curl.h>
 
@@ -21,29 +22,33 @@ size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
 }
 
 // Retrieve cookies and crumb from yahoo finance site to download csv
-char get_crumb_and_cookies(char *symbol, char cookiefilename[FILENAME_MAX], char crumbfilename[FILENAME_MAX]) {
+const char* get_crumb_and_cookies(char *symbol, char cookiefilename[FILENAME_MAX], char crumbfilename[FILENAME_MAX]) {
     
     // Init necessary variables
     CURL *cookiecurl = curl_easy_init();
     FILE * crumbfile;
     char nline[256];
+    const char *crumb = "";
     
     // Init curl session
     cookiecurl = curl_easy_init();
     
     // Create URL for the Yahoo Finance history for that stock
-    string cookie_url = string("https://finance.yahoo.com/quote/") + string(symbol) + string("/history?p=") + string(symbol);
-
+    //string cookie_url = string("https://finance.yahoo.com/quote/") + string(symbol) + string("/history?p=") + string(symbol);
+    string cookie_url = string("https://finance.yahoo.com");
+    cout << endl << cookie_url << endl;
+    
     // Set URL to go to finance.yahoo.com and prepare cookies file
     curl_easy_setopt(cookiecurl, CURLOPT_URL, cookie_url.c_str());
     curl_easy_setopt(cookiecurl, CURLOPT_VERBOSE, 1L);
     curl_easy_setopt(cookiecurl, CURLOPT_COOKIEFILE, cookiefilename);
     curl_easy_setopt(cookiecurl, CURLOPT_COOKIELIST, "ALL");
     
-    // HTTP-header style cookie format
-    snprintf(nline, sizeof(nline),
-             "Set-Cookie: OLD_PREF=3d141414bf4209321; "
-             "expires=Sun, 17-Jan-2038 19:14:07 GMT; path=/; domain=.example.com");
+    // Netscape format cookie 
+    snprintf(nline, sizeof(nline), "%s\t%s\t%s\t%s\t%lu\t%s\t%s",
+             ".example.com", "TRUE", "/", "FALSE",
+             (unsigned long)time(NULL) + 31337UL,
+             "PREF", "hello example, i like you very much!");
     
     // Set file as cookie jar
     curl_easy_setopt(cookiecurl, CURLOPT_COOKIEJAR, cookiefilename);
@@ -61,7 +66,7 @@ char get_crumb_and_cookies(char *symbol, char cookiefilename[FILENAME_MAX], char
         
         // Performs blocking file transfer as defined
         curl_easy_perform(cookiecurl);
-        
+
         // Close header file
         fclose(crumbfile);
     }
@@ -70,8 +75,19 @@ char get_crumb_and_cookies(char *symbol, char cookiefilename[FILENAME_MAX], char
     curl_easy_cleanup(cookiecurl);
     curl_global_cleanup();
     
-    char cx;
-    return cx;
+    // Search for crumb in newly created crumb file
+    ifstream searchFile(crumbfilename);
+    string str;
+    while (getline(searchFile, str)) {
+        if (str.find("{\"site\":\"fpctx\",\"crumb\":\"") != str.npos){
+            size_t pos = str.find("{\"site\":\"fpctx\",\"crumb\":\"") + 25;
+            crumb = str.substr(pos, 11).c_str();
+            break;
+        }
+    }
+    
+    cout << endl << crumb << endl;
+    return crumb;
 }
 
 
@@ -84,8 +100,10 @@ YahooFinanceCSVReader:: YahooFinanceCSVReader(char *symbol, char *startdate, cha
     FILE *fp;
     
     // Get crumb and use it to create the download url
-    char crumb = get_crumb_and_cookies(symbol, cookiefilename, crumbfilename);
-    string down_url = string("https://query1.finance.yahoo.com/v7/finance/download/") + string(symbol) + string("?period1=") + string(startdate) + string("&period2=") + string(enddate) + string("&interval=") + string(interval) + string("&events=history&crumb=") + string(&crumb);
+    const char *crumb = get_crumb_and_cookies(symbol, cookiefilename, crumbfilename);
+    string down_url = string("https://query1.finance.yahoo.com/v7/finance/download/") + string(symbol) + string("?period1=") + string(startdate) + string("&period2=") + string(enddate) + string("&interval=") + string(interval) + string("&events=history&crumb=") + string(crumb);
+    
+    cout << endl << down_url << endl;
     
     // Init the curl session
     curl = curl_easy_init();
@@ -95,6 +113,8 @@ YahooFinanceCSVReader:: YahooFinanceCSVReader(char *symbol, char *startdate, cha
     
     // Set URL to go to given URL
     curl_easy_setopt(curl, CURLOPT_URL, down_url.c_str());
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_NOBODY, 0L);
     
     // Send all data to this function
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
